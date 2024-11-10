5299cb52e02f0e356a94756d2823d03a40dc6003
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-10 15:47:22 +0530

    Create Routes folder and user router

--- api/Index.js
+++ api/Index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+
+
 dotenv.config();
 
 
@@ -18,3 +21,5 @@ mongoose.connect(process.env.MONGO)
 app.listen(3000, () => {
     console.log('server is running on port 3000')
 })
+
+app.use('/api/user',userRoutes)
