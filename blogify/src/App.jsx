b5b7bf217ae677055fc55da22be9024d08573783
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-10 12:54:49 +0530

    Add header component

--- blogify/src/App.jsx
+++ blogify/src/App.jsx
@@ -6,9 +6,11 @@ import Signup from './Pages/Signup';
 import Dashboard from './Pages/Dashboard';
 import About from './Pages/About';
 import Projects from './Pages/Projects'
+import Header from './Components/Header';
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signin' element={<Signin/>}/>
