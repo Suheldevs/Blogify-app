997a0906dd5d219dd6009e875df107777930de4b
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-10 17:22:20 +0530

    Add a middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username == '' || email == '' || password == '') {
@@ -14,6 +14,6 @@ export const signup = async (req, res) => {
         res.status(200).json({ message: 'signin successful' })
     }
     catch (err) {
-        res.status(500).json({ message: err.message })
+       next(err)
     };
 }
