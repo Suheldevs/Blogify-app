cfa89164f4e7dab0c655d68e7f5f98eaa52f2c20
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-17 11:29:13 +0530

    install Redux Persist
    Step1:npm i redux-persist(install redux-persist for localicy store our currect user if user refress the page its contain user data);
    step2:update store.js file.
    step3: update main.jsx file

--- blogify/src/redux/store.js
+++ blogify/src/redux/store.js
@@ -1,7 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Combine reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// Configure persistence
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store
 export const store = configureStore({
-  reducer: {
-    user:userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable checks for redux-persist
+    }),
+});
+
+// Export persistor
+export const persistor = persistStore(store);
