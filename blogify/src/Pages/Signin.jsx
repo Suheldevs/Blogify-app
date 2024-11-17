c151b5ea1e9aa61dcf1470ecfa7b7bbbb26c8d7f
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-17 10:01:18 +0530

    Redux setup
    step1:install
    ste2:create redux folder in side redux folder create a store.js file
    step3:create a userSlice file and then import it on store.js file

--- blogify/src/Pages/Signin.jsx
+++ blogify/src/Pages/Signin.jsx
@@ -46,7 +46,7 @@ function Signin() {
     }
   }
   return (
-    <div className=' lg:mt-28 mt-12'>
+    <div className=' lg:mt-28 mt-12 h-[75vh]'>
       <div className='flex p-3 max-w-6xl mx-auto flex-col  md:flex-row md:items-center gap-10'>
       {/* left */}
       <div className='flex-1'>
--- blogify/src/Pages/Signup.jsx
+++ blogify/src/Pages/Signup.jsx
@@ -44,7 +44,7 @@ function Signup() {
     }
   }
   return (
-    <div className=' lg:mt-28 mt-12'>
+    <div className=' lg:mt-28 mt-12 h-[75vh]'>
       <div className='flex p-3 max-w-6xl mx-auto flex-col  md:flex-row md:items-center gap-10'>
       {/* left */}
       <div className='flex-1'>
