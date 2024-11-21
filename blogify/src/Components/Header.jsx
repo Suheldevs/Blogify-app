609ec20509a686d93d0b6522ada51a6ea55995f4
MOHDSUHEL_01 <mohdsuhail16289@gmail.com>
2024-11-21 22:19:16 +0530

    dark mood and light mood funtinality

--- blogify/src/Components/Header.jsx
+++ blogify/src/Components/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import{ Avatar, Button, Dropdown, DropdownDivider, Navbar, TextInput} from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai';
-import { FaMoon } from "react-icons/fa";
-import {useSelector} from 'react-redux';
+import { FaMoon ,FaSun} from "react-icons/fa";
+import {useSelector,useDispatch} from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
+
 function Header() {
     const path = useLocation().pathname;
     const {currentUser} =useSelector(state =>state.user);
+    const {theme} = useSelector((state)=>state.theme);
+    const dispatch = useDispatch();
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-lg lg:text-xl font-semibold dark:text-white'>
@@ -24,8 +28,8 @@ function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-3 md:order-2'>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                <FaMoon/>
+            <Button className='w-12 h-10 ' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+              {theme === 'light' ? <FaMoon/> : <FaSun/>}  
             </Button>
             <Link to='/signin'>
             {currentUser?(
